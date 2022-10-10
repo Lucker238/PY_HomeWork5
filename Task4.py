82c90c705724c6da097b57fd7ba470bc67c85205
Maksim Sinikov <md.sinikov@gmail.com>
2022-10-10 21:38:08 +0500

    Добавил файлы в 4 задаче

--- Task4.py
+++ Task4.py
@@ -1,6 +1,5 @@
 # Например, строчку aaababbcbbb он переводит в (a, 3) (b, 1) (a, 1) (b, 2) (c, 1) (b, 3).
 
-text = 'aaababbcbbbzzzzzaaaaaaabbywwwwwwwwwww'
 
 def coding(text: str):
     archive = [('just for volume',100500)]
@@ -24,6 +23,30 @@ def decoding(arch: list):
         decode += i[0] * i[1]
     return decode
 
+path_in = 'C:/Users/User/Desktop/Geekbrains/Python/HW5/input.txt'
+inp = open(path_in, 'r')
+text = inp.read()
+inp.close()
+
+coding_text = coding(text)
+
+path_out = 'C:/Users/User/Desktop/Geekbrains/Python/HW5/output.txt'
+out = open(path_out, 'w')
+out.write(str(coding_text))
+out.close()
+
+one_more_out = open(path_out, 'r')
+text_for_decoding = one_more_out.read()
+
+
+text_for_decoding = text_for_decoding[2:len(text_for_decoding)-2]
+text_for_decoding = text_for_decoding.replace("'",'')
+list_for_decoding = text_for_decoding.split('), (')
+one_more_list = []
+for i in list_for_decoding:
+    k = i.split(', ')
+    one_more_list.append((str(k[0]), int(k[1])))
+
 print(text)
-print(coding(text))
-print(decoding(coding(text)))
+print(coding_text)
+print(decoding(one_more_list))
